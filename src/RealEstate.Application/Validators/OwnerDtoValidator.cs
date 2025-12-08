957801f46ec27d95de06d1dc499d9ef3d679dc2a
Jan Michael Sanchez Sierra <68264772+maicol9310@users.noreply.github.com>
2025-12-08 04:38:34 -0500

    feat(application): A validator is added to properties and owner

---
+++ src/RealEstate.Application/Validators/OwnerDtoValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using RealEstate.Contracts.DTOs;
+
+namespace Hotels.Application.Validators
+{
+    public class OwnerDtoValidator : AbstractValidator<OwnerDto>
+    {
+        public OwnerDtoValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required.");
+            RuleFor(x => x.Address).NotEmpty().WithMessage("Address is required.");
+            RuleFor(x => x.Photo).NotEmpty().WithMessage("Photo is required.");
+        }
+    }
+}
